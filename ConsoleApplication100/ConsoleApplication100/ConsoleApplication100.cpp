d22e83c9fa6cfcea154856d3e5cb8eed6ef0241f
Dima <dima-kuharchik@mail.ru>
2016-02-16 10:24:47 +0300

    newproject

---
+++ ConsoleApplication100/ConsoleApplication100/ConsoleApplication100.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication100.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
